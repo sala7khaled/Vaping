0e82727eeb7692bba6c1d141d696c27bac855694
sala7khaled <sala7khaled7@gmail.com>
2022-12-16 21:04:23 +0200

    Product model enhanced

--- Vaping/Core/Network/Model/Product.swift
+++ Vaping/Core/Network/Model/Product.swift
@@ -6,49 +6,29 @@
 //  Copyright © 2022 Salah Khaled. All rights reserved.
 //
 
-//import Foundation
-//
-//class Product: NSObject, NSCoding, Codable {
-//
-//    var id: Int
-//    var desc: String
-//    var image: Image
-//    var price: Int
-//
-//    func encode(with coder: NSCoder) {
-//        coder.encode(id, forKey: "id")
-//        coder.encode(desc, forKey: "productDescription")
-//        coder.encode(image, forKey: "image")
-//        coder.encode(price, forKey: "price")
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        self.id = coder.decodeObject(forKey: "id") as? Int ?? 0
-//        self.desc = coder.decodeObject(forKey: "productDescription") as? String ?? ""
-//        self.image = coder.decodeObject(forKey: "image") as? Image ?? Image(width: 0, height: 0, url: "")
-//        self.price = coder.decodeObject(forKey: "price") as? Int ?? 0
-//
-//    }
-//}
-//
-//extension Product {
-//
-//    enum CodingKeys: String, CodingKey {
-//
-//        case id
-//        case desc = "productDescription"
-//        case image
-//        case price
-//    }
-//}
+import Foundation
 
-
-struct Product: Codable {
+class Product: NSObject, NSCoding, Codable {
 
     var id: Int
     var desc: String
     var image: Image
     var price: Int
+
+    func encode(with coder: NSCoder) {
+        coder.encode(id, forKey: "id")
+        coder.encode(desc, forKey: "productDescription")
+        coder.encode(image, forKey: "image")
+        coder.encode(price, forKey: "price")
+    }
+
+    required init?(coder: NSCoder) {
+        self.id = coder.decodeObject(forKey: "id") as? Int ?? 0
+        self.desc = coder.decodeObject(forKey: "productDescription") as? String ?? ""
+        self.image = coder.decodeObject(forKey: "image") as? Image ?? Image(width: 0, height: 0, url: "")
+        self.price = coder.decodeObject(forKey: "price") as? Int ?? 0
+
+    }
 }
 
 extension Product {
